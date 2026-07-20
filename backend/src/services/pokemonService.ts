237918cb014cbe3bb7458c5e6734a1ece5aecb7b
LuisFelipeTG96 <54545381+LuisFelipeTG96@users.noreply.github.com>
2026-07-20 16:51:08 -0500

    feat: add transformPokemonData function

--- backend/src/services/pokemonService.ts
+++ backend/src/services/pokemonService.ts
@@ -19,3 +19,28 @@ export async function fetchPokemonSpeciesData(pokemonId: number) {
         throw error;
     }
 }
+
+export async function transformPokemonData(pokemonId: number) {
+
+    try{
+        const pokemonData = await fetchPokemonData(pokemonId);
+        const speciesData = await fetchPokemonSpeciesData(pokemonId);
+
+        const transformedData = {
+            id: pokemonData.id,
+            name: pokemonData.name,
+            pokedexDescription: speciesData.flavor_text_entries.find((entry: any) => entry.language.name === 'en')?.flavor_text || 'No description available.',
+            pokedexNumber: speciesData.pokedex_numbers.find((entry: any) => entry.pokedex.name === 'national')?.entry_number || pokemonData.id,
+            spriteUrl: pokemonData.sprites.front_default,
+            homeUrl: pokemonData.sprites.other.home.front_default,
+            types: pokemonData.types.map((typeInfo: any) => typeInfo.type.name),
+            generation: Number(speciesData.generation.url.split('/').filter(Boolean).pop()),
+        }
+        
+        return transformedData;
+
+    } catch (error) {
+        console.error(`Error transforming data for Pokémon ID ${pokemonId}:`, error);
+        throw error;
+    }
+}
